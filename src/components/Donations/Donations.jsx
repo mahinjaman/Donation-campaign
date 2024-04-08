fb203012aeeaef10ae60a0e1ae4943c58c3de1a7
mahinjaman07 <mahinjaman434@gmail.com>
2024-04-09 01:58:41 +0600

    Donate card add to local storage and this card will be showing donations page

--- src/components/Donations/Donations.jsx
+++ src/components/Donations/Donations.jsx
@@ -1,16 +1,35 @@
 import { Helmet } from "react-helmet-async";
-import { useLoaderData } from "react-router-dom";
 import { getDataFromLS } from "../../Utiliti/LocalStorage";
 import DonationStoredCard from "../DonationStoredCard/DonationStoredCard";
+import { useEffect, useState } from "react";
 
 const Donations = () => {
-    const AllDonations = useLoaderData();
+    const [AllDonations, setAllDonations] = useState([]);
+    const [displayStoredCards, setDisplayStoredCards] = useState([]);
+    const [displayStoresDonationLength, setDisplayStoresDonationLength] = useState(4);
+    useEffect(()=>{
+        fetch('donationsData.json')
+        .then(res=> res.json())
+        .then(data => setAllDonations(data))
+    },[])
     const storedDonationsId = getDataFromLS();
-    const storedDonationsCard = [];
-    for(const id of storedDonationsId) {
-        storedDonationsCard.push(AllDonations.find(donation => donation.id === id));
+    useEffect(()=>{
+        const storedDonationsCard = [];
+        if (AllDonations.length) {
+            for (const id of storedDonationsId) {
+                storedDonationsCard.push(AllDonations.find(donation => donation.id === id));
+            }
+            
+        }
+        setDisplayStoredCards(storedDonationsCard)
+    },[AllDonations])
+
+
+    const handleShowDonation = () => {
+        setDisplayStoresDonationLength(displayStoredCards.length);
     }
-    console.log(storedDonationsCard);
+
+    const displayDonateCardsSpecific = displayStoredCards.slice(0, displayStoresDonationLength)
     return (
         <div className="pb-10">
             <Helmet>
@@ -18,10 +37,16 @@ const Donations = () => {
             </Helmet>
             <div className=" grid md:grid-cols-2 gap-4 lg:grid-cols-1 xl:grid-cols-2 md:w-10/12 mx-auto md:mt-20">
                 {
-                    storedDonationsCard.map(card => <DonationStoredCard key={card.id} card={card}></DonationStoredCard>)
+                    displayDonateCardsSpecific.map(card => <DonationStoredCard key={card.id} card={card}></DonationStoredCard>)
                 }
             </div>
+            <div className={`flex mt-10 items-center justify-center ${storedDonationsId.length > 4 ? 'static' : 'hidden'}`}>
+                <button onClick={handleShowDonation} className={`bg-green-600 text-white font-semibold py-2 px-5 rounded-md ${displayStoresDonationLength === displayStoredCards.length ? 'hidden' : 'static'}`}>
+                    Show All
+                </button>
+            </div>
         </div>
+        
     );
 };
 
